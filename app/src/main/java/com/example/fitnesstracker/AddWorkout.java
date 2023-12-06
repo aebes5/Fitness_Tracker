89afc9e783eccaf38dd5ac8da7f0eb48148f6b4b
tcousik <119012703+tcousik@users.noreply.github.com>
2023-12-05 20:56:07 -0700

    calories burned addition

--- app/src/main/java/com/example/fitnesstracker/AddWorkout.java
+++ app/src/main/java/com/example/fitnesstracker/AddWorkout.java
@@ -15,7 +15,7 @@ public class AddWorkout extends DialogFragment {
     private ActivityAddWorkoutBinding binding;
 
     @Override
-    public Dialog onCreateDialog (Bundle savedInstanceState) {
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
 
         binding = ActivityAddWorkoutBinding.inflate(LayoutInflater.from(getContext()));
 
@@ -41,32 +41,44 @@ public class AddWorkout extends DialogFragment {
         return builder.create();
     }
 
-    public void save(){
+    public void save() {
         String name = binding.editTextText.getText().toString();
         int duration = Integer.parseInt(binding.editTextText2.getText().toString());
         String type = "";
 
-        if(binding.radioButton3.isChecked()){
+        if (binding.radioButton3.isChecked()) {
             type = "Cardio";
-        }
-        else if(binding.radioButton4.isChecked()){
+        } else if (binding.radioButton4.isChecked()) {
             type = "Strength Training";
         }
+
         Workout workout = new Workout(name, type, duration);
-        WorkoutTracker workoutTracker = (WorkoutTracker)getActivity();
+        WorkoutTracker workoutTracker = (WorkoutTracker) getActivity();
         workoutTracker.addWorkoutToList(workout);
 
         SharedPreferences sharedPreferences = getActivity().getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
         int currentWorkouts = sharedPreferences.getInt("workouts", 0);
         int updatedWorkouts = currentWorkouts + 1;
 
+        int currentCaloriesBurned = sharedPreferences.getInt("caloriesBurned", 0);
+        int updatedCaloriesBurned;
+
+        if (type.equals("Cardio")) {
+            updatedCaloriesBurned = currentCaloriesBurned + (duration * 10);
+        } else {
+            // Assuming type is "Strength Training"
+            updatedCaloriesBurned = currentCaloriesBurned + (duration * 5);
+        }
+
         SharedPreferences.Editor editor = sharedPreferences.edit();
         editor.putInt("workouts", updatedWorkouts);
+        editor.putInt("caloriesBurned", updatedCaloriesBurned);
         editor.apply();
 
         dismiss();
     }
-    public void clear(){
+
+    public void clear() {
         binding.editTextText.setText("");
         binding.radioButton4.setChecked(true);
     }
